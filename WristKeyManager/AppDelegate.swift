f3b9608f5d1bc6ef2e5ba0904f3005200eba51d7
Sreeteja Jonnada <sreetejajonnada@live.com>
2018-08-08 20:33:50 +0100

    Initial Commit

---
+++ WristKeyManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WristKeyManager
+//
+//  Created by Sreeteja Jonnada on 08/08/2018.
+//  Copyright © 2018 Sreeteja Jonnada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WristKeyManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WristKeyManager
+//
+//  Created by Sreeteja Jonnada on 08/08/2018.
+//  Copyright © 2018 Sreeteja Jonnada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
